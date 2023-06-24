a6c7504ace16a3019bf84eb83fd1c3bea2f5a154
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2023-06-24 15:27:04 +0100

    trpc router

--- src/server/api/routers/todo.ts
+++ src/server/api/routers/todo.ts
@@ -33,7 +33,44 @@ all: protectedProcedure.query(async({ctx}) => {
         }
     ]
   }),
-  create: protectedProcedure.input().mutation(async({ctx}) =>{
+  create: protectedProcedure.input(todoInput).mutation(async({ctx,input}) =>{
+    return ctx.prisma.todo.create({
+        data:{
+            text:input,
+            user:{
+                connect:{
+                    id:ctx.session.user.id
+                }
+            }
+        }
+    })
+    
+  }),
+  delete: protectedProcedure
+  .input(z.string())
+  .mutation(async({ctx,input}) =>{
+    return ctx.prisma.todo.delete({
+            where:{
+                id:input
+        }
+    })
+    
+  }),
+  toggle: protectedProcedure
+  .input(z.object({
+    id:z.string(),
+    done: z.boolean()
 
+  }))
+  .mutation(async({ctx,input: {id,done}}) =>{
+    return ctx.prisma.todo.update({
+            where:{
+                id,
+        },
+        data:{
+            done,
+        }
+    })
+    
   })
 });
